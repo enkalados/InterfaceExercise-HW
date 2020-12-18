e1e70dea91d5a62073622c8bcf439091f385df41
enkalados <yakup_mknc@hotmail.com>
2020-12-18 15:45:55 +0300

    Finish

---
+++ Scripts-YakupMakinaci/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 500;
+    private Rigidbody rigidbody;
+    public Rigidbody Rigidbody { get { return (rigidbody == null) ? rigidbody = GetComponent<Rigidbody>() : rigidbody; } }
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+    private void PlayerMove()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Rigidbody.velocity = input * moveSpeed * Time.fixedDeltaTime; //Hız ile yönlendirme
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Etkileşim");
+        IInteractable myInteract = other.GetComponent<IInteractable>();
+        if (myInteract != null)
+        {
+            other.GetComponent<IInteractable>().Interact();
+        }
+    }
+}
